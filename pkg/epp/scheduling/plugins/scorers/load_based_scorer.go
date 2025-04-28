b76d8a00968b0f9f0723fdde34d23880b886eb01
Maya Barnea <mayab@il.ibm.com>
2025-04-28 15:39:06 +0300

    Added scorer based on pod's load (length of waiting requests queue)

---
+++ pkg/epp/scheduling/plugins/scorers/load_based_scorer.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package scorers
+
+import (
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/config"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/plugins"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/types"
+)
+
+type LoadBasedScorer struct {
+}
+
+func NewLoadBasedScorer() plugins.Scorer {
+	return LoadBasedScorer{}
+}
+
+func (s LoadBasedScorer) Name() string {
+	return "load based scorer"
+}
+
+// Score scores the given pod in range of 0-1
+// Currently metrics contains number of requests waiting in the queue, there is no information about number of requests
+// that can be processed in the given pod immediately.
+// Pod with empty waiting requests queue is scored with 0.5
+// Pod with requests in the queue will get score between 0.5 and 0.
+// Score 0 will get pod with number of requests in the queue qual to the threshold used in load-based filter (QueueingThresholdLoRA)
+// In future pods with additional capacity will get score higher than 0.5
+func (s LoadBasedScorer) Score(ctx *types.SchedulingContext, pod types.Pod) float64 {
+	waitingRequests := float64(pod.GetMetrics().WaitingQueueSize)
+	score := 0.0
+
+	if waitingRequests == 0 {
+		score = 0.5
+	} else {
+		score = 0.5 * (1.0 - (waitingRequests / float64(config.Conf.QueueingThresholdLoRA)))
+	}
+
+	return score
+}
