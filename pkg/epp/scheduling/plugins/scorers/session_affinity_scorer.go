13d433b47c49e35c4adac913c624064af2ccdb05
Dmitri Pikus <DPIKUS@il.ibm.com>
2025-04-30 12:27:55 +0300

    Partial implementation of Session Affinity (without headers in response)

---
+++ pkg/epp/scheduling/plugins/scorers/session_affinity_scorer.go
@@ -0,0 +1,82 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package scorers
+
+import (
+	"strings"
+	"sync"
+	"time"
+
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/plugins"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/types"
+)
+
+const (
+	sessionKeepAliveTime           = 60 * time.Minute // How long should an idle session be kept alive
+	sessionKeepAliveCheckFrequency = 15 * time.Minute // How often to check for overly idle sessions
+	sessionIDHeader                = "Session-ID"     // hame of the session header in request
+	notSecretKey                   = "my_not_secret_key"
+)
+
+// sessionAffinity is a routing scorer that routes subsequent
+// requests in a session to the same pod as the first request in the
+// session was sent to, by giving that pod the specified weight and assigning
+// zero score to the rest of the targets
+type SessionAffinityScorer struct {
+	sessions *sync.Map
+}
+
+func NewSessionAffinityScorer() plugins.Scorer {
+	s := &SessionAffinityScorer{
+		sessions: &sync.Map{},
+	}
+	return s
+}
+
+func (s *SessionAffinityScorer) Name() string {
+	return "session affinity scorer"
+}
+
+func (s *SessionAffinityScorer) Score(ctx *types.SchedulingContext, pods []types.Pod) map[types.Pod]float64 {
+	scoredPods := make(map[types.Pod]float64)
+
+	reqHeaders := ctx.Req.Headers
+
+	var sessionId = ""
+	for k, v := range reqHeaders {
+		if strings.EqualFold(k, sessionIDHeader) {
+			sessionId = v
+		}
+	}
+
+	for _, pod := range pods {
+		if sessionId == "" {
+			scoredPods[pod] = 0.0
+		} else {
+			if pod.GetPod().NamespacedName.String() == xorEncryptDecrypt(sessionId, notSecretKey) {
+				scoredPods[pod] = 1.0
+			}
+		}
+	}
+
+	return scoredPods
+}
+
+func xorEncryptDecrypt(input, key string) string {
+	output := make([]byte, len(input))
+	for i := range input {
+		output[i] = input[i] ^ key[i%len(key)]
+	}
+	return string(output)
+}
