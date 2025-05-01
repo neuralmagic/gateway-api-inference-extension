00eaf5e6f9c8ccea28d51012f02ff0dd1ba8e2c4
Andrew Anderson <andy@clubanderson.com>
2025-04-30 20:53:49 -0400

    fix lint issues

--- pkg/epp/server/runserver_test.go
+++ pkg/epp/server/runserver_test.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package server_test
 
 import (
@@ -24,6 +23,9 @@ import (
 	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/server"
 	logutil "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/util/logging"
 )
+
+// Define a variable with the manager package type to explicitly show usage to linter
+var _ manager.LeaderElectionRunnable = nil
 
 func TestRunnable(t *testing.T) {
 	// Make sure AsRunnable() does not use leader election.
